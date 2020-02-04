519fa6c16373c430c83385d4927c843d889604e1
Maxboo <mbourrier@booprod.com>
2020-02-04 13:55:58 +0100

    bdd access

--- pages/Fonctions.php
+++ pages/Fonctions.php
@@ -9,7 +9,7 @@
 //Récupérer entre parenthèse
 //https://www.developpez.net/forums/d1469403/php/langage/recuperer-chaine-entre-parentheses/
 //https://www.developpez.net/forums/d812317/bases-donnees/oracle/outils/sql-plus/connaitre-type-champs-d-table/
-/*
+
 function getDataBase() {
     try {
         $bdd = new PDO('mysql:host=mysql.montpellier.epsi.fr;dbname=bddneptune;charset=utf8;port=5206;',
@@ -20,8 +20,8 @@ function getDataBase() {
     }
     return $bdd;
 }
-*/
 
+/*
 function getDataBase() {
     try {
         $bdd = new PDO('mysql:host=localhost;dbname=bddneptune;charset=utf8',
@@ -32,7 +32,7 @@ function getDataBase() {
     }
     return $bdd;
 }
-
+*/
 
 function getListe(PDO $bdd,$fromTable,Array $cond = [],Array $condLike = [],$askSelect = '*') { //Cond pour Condition
     //Pour utiliser cette fonction il faut lui envoyer :
