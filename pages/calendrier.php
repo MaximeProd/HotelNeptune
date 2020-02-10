042670bec9368db7b14ec56a74537b98b7a64e93
Maxboo <mbourrier@booprod.com>
2020-02-10 14:31:36 +0100

    calendrier démo ok

--- pages/calendrier.php
+++ pages/calendrier.php
@@ -21,8 +21,24 @@ function calendar ($bdd,$m, $y)
     $mois = array('','Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
     $week = array('lu','ma','me','je','ve','sa','di');
 
-    $t = mktime(12, 0, 0, $m, 1, $y); // Timestamp du premier jour du mois
 
+    $t = mktime(12, 0, 0, $m, 1, $y); // Timestamp du premier jour du mois
+    for ($l = 0 ; $l < 6 ; $l++) // calendrier sur 6 lignes
+    {
+        for ($i = 0; $i < 7; $i++) // 7 jours de la semaine
+        {
+            $w = $sem[(int)date('w', $t)]; // Jour de la semaine à traiter
+            $m2 = (int)date('n', $t); // Tant que le mois reste celui du départ
+
+            $demain = date('Y-m-d', strtotime(date('Y-m-d') . ' +1 days'));
+            if (($w == $i) && ($m2 == $m)) // Si le jours de semaine et le mois correspondent
+            {
+
+                $t += 86400; // Passe au jour suivant
+            }
+        }
+    }
+    $t = mktime(12, 0, 0, $m, 1, $y); // Timestamp du premier jour du mois
     echo '
 
 <table><tbody>';
@@ -55,13 +71,19 @@ function calendar ($bdd,$m, $y)
             {
                 $date= $y.'-'.date('m',$t).'-'.date('d',$t);
                 $listeReserv = getListe($bdd,"planning",Array("chambre_id"=>1,"jour"=>$date));
-                $id = 'id="open"';
+                $id = 'toggle'.$t.'';
                 if (!empty($listeReserv)){
-                    $id = 'id="lock"';
+                    $id = '"lock"';
                     var_dump($id);
                 }
                 $var = date('j',$t);
-                echo '<label for="'.$date.'"><td '.$id.'>'.$var.'</td></label><input type="checkbox" class="test" name="'.$date.'">'; // Affiche le jour du mois
+                echo '   
+                <td>
+                  <input id="'.$id.'" type="checkbox">
+                  <label for="toggle'.$t.'">'.$var.'</label>
+                </td>
+                '  ;// Affiche le jour du mois
+
                 $t += 86400; // Passe au jour suivant
             }
             else
@@ -72,6 +94,7 @@ function calendar ($bdd,$m, $y)
         echo '</tr>';
     }
     echo '</tbody></table>';
+
 }
 
 calendar($bdd,01,2020)
