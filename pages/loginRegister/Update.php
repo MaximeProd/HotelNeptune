1d9916131e70d3da8c9fc6dac8c746360fb4942c
SebastianRodCimas <56884033+SebastianRodCimas@users.noreply.github.com>
2020-12-08 01:21:51 +0100

    test

--- pages/loginRegister/Update.php
+++ pages/loginRegister/Update.php
@@ -4,7 +4,7 @@ require '../Fonctions.php';
 $bdd = getDataBase();
 if (isset($bdd)){
     if (isset($_POST)){
-        updateListe()($bdd,'etudiant',$_POST,"id".$_SESSION['id']);
+        updateListe($bdd,'etudiant',$_POST,"id".$_SESSION['id']);
         $_SESSION["erreur"] = "Compte modifié avec succés";
     }
 } else {
